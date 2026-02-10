43ddd724626f26baa65e18ba24902a137c46b2c6
Kelventz <92531379+Kelventz@users.noreply.github.com>
2026-02-10 20:24:41 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositorio de treino
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Bracim ta troll</h1>
+function Home() {
+  return <h1>Bracim ta troll</h1>;
 }
 
 export default Home;
